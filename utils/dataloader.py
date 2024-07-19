20caf895ee3e47412311f78aa5935da06cce55fe
AlexKenna <AlexKenna04@gmail.com>
2024-07-19 23:59:43 +1000

    Added train/val/test split to dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,30 +1,54 @@
 import numpy as np
 import h5py
 import pickle
+
+from enum import Enum
+
 import torch
 from torch.utils.data import Dataset
 
 
+class DatasetType(Enum):
+    TRAIN = 1
+    TEST = 2
+    VALIDATION = 3
+
+
 class FluidDataset(Dataset):
-    def __init__(self, filename: str, train: bool = True):
-        self.filename = filename
-        self.train = train
+    def __init__(self, file_name: str, dataset_type: DatasetType = DatasetType.TRAIN):
+        self.file_name = file_name
+        self.dataset_type = dataset_type
+
+        self._train_len = 1500
+        self._val_len = 250
+        self._test_len = 250
 
     def __len__(self):
-        return 1500 if self.train else 500
+        if self.dataset_type == DatasetType.TRAIN:
+            return self._train_len
+        elif self.dataset_type == DatasetType.VALIDATION:
+            return self._val_len
+        elif self.dataset_type == DatasetType.TEST:
+            return self._test_len
 
     def __getitem__(self, idx):
-        starting_pos = 0 if self.train else 1500
 
-        with h5py.File(f"{self.filename}.h5", "r") as datafile:
+        if self.dataset_type == DatasetType.TRAIN:
+            starting_pos = 0
+        elif self.dataset_type == DatasetType.VALIDATION:
+            starting_pos = self._train_len
+        elif self.dataset_type == DatasetType.TEST:
+            starting_pos = self._train_len + self._val_len
+
+        with h5py.File(f"{self.file_name}.h5", "r") as datafile:
 
             # Convert data to correct format
             data = torch.tensor(datafile["dataset"][starting_pos + idx])
             data = data.type(torch.float)
-            data = data.permute(2, 3, 0, 1)
+            data = data.permute(2, 3, 1, 0)
 
             # Construct grid
-            with open(f"{self.filename}.pkl", 'rb') as handle:
+            with open(f"{self.file_name}.pkl", "rb") as handle:
                 info = pickle.load(handle)
                 grid = np.array(np.meshgrid(info["coords"]["x"], info["coords"]["y"]))
                 grid = torch.tensor(grid).type(torch.float)
